7d6e7746374505ca2706d231c837df44e7a192ec
Vahe Abovyan <vahe.abov@gmail.com>
2023-03-09 15:50:15 +0400

    fix main banner

--- src/components/shared/MainBanner.tsx
+++ src/components/shared/MainBanner.tsx
@@ -1,20 +1,29 @@
-import { Box, Image, Grid, Text, Heading, Button, Link, Stack, Container } from '@chakra-ui/react';
+import { Box, Text, Heading, Button, Stack, Container } from '@chakra-ui/react';
 import bannerImage from '../../assets/banner.jpg';
 import { Link as RouterLink } from 'react-router-dom';
 
-const MainBanner: React.FC = () => {
+
+interface MainBannerProps {
+  title: string;
+  subtitle: string;
+  button: string;
+  href: string;
+}
+
+
+
+const MainBanner: React.FC<MainBannerProps> = ({ title, subtitle, href, button }) => {
   return (
-    <Box position="relative" height="650px" width="100%">
-      <Image src={bannerImage} height="100%" width="100%" objectFit="cover" />
-      <Container maxW="1440px" mx="auto">
-        <Stack position="absolute" top="40%" px={4} py={6} spacing={6} direction="column" rounded="md" backgroundColor="whiteAlpha.800">
+    <Box position="relative" height="650px" width="100%" backgroundImage={bannerImage} backgroundSize="cover" backgroundPosition="center" display="flex" justifyContent="center" alignItems="center">
+      <Container maxW="650px" mx="auto">
+        <Stack px={4} py={6} spacing={6} direction="column" rounded="md" backgroundColor="whiteAlpha.800" textAlign="center">
           <Box display="flex" flexDirection="column" gap={6}>
-            <Heading as="h1" fontSize={48} color="gray.600">Welcome to Foozball!</Heading>
-            <Text fontSize={24} color="gray.600">Start your adventure in the world of football now!</Text>
+            <Heading as="h1" fontSize={48} color="gray.600">{title}</Heading>
+            <Text fontSize={24} color="gray.600">{subtitle}</Text>
           </Box>
-          <RouterLink to='/countries'>
+          <RouterLink to={href}>
             <Button fontSize={20} colorScheme="orange" alignSelf="baseline" size="lg">
-              Explore countries
+              {button}
             </Button>
           </RouterLink>
         </Stack>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Image, Grid, Text, Heading, Button, Link, Stack, Container } from '@chakra-ui/react';
+import { Box, Image, Grid, GridItem, Text, Heading, Button, Link, Stack } from '@chakra-ui/react';
 import CustomContainer from '../../components/shared/CustomContainer';
 import MainBanner from '../../components/shared/MainBanner';
 
@@ -6,9 +6,20 @@ import MainBanner from '../../components/shared/MainBanner';
 const Home: React.FC = () => {
   return (
     <Box>
-      <MainBanner />
+      <MainBanner title="Welcome to Foozball!" subtitle="Start your adventure in the world of football now!" button="Explore countries" href="/countries" />
       <CustomContainer>
-        <h1>Hello</h1>
+        <Stack pt={4} direction="column">
+          <Heading as="h2" my={4} textAlign="center">Explore FoozBall!</Heading>
+          <Grid h='600px'
+            templateRows='repeat(2, 1fr)'
+            templateColumns='repeat(5, 1fr)'
+            gap={4}>
+            <GridItem rowSpan={2} colSpan={1} bg='tomato' />
+            <GridItem colSpan={2} bg='papayawhip' />
+            <GridItem colSpan={2} bg='papayawhip' />
+            <GridItem colSpan={4} bg='tomato' />
+          </Grid>
+        </Stack>
       </CustomContainer>
     </Box>
   );
